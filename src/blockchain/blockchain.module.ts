c3b39fe5404f4c3b43790dcc8fe0d9c490d95903
강백호 <kbh081004@gmail.com>
2025-08-11 17:11:33 +0900

    feat: 투표이벤트 감지후 DB 저장기능 추가

--- src/blockchain/blockchain.module.ts
+++ src/blockchain/blockchain.module.ts
@@ -4,9 +4,10 @@ import { BlockchainController } from './blockchain.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Game } from 'src/game/entity/game.entity';
 import { User } from 'src/auth/entity/user.entity';
+import { Vote } from 'src/game/entity/vote.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Game, User])],
+  imports: [TypeOrmModule.forFeature([Game, User, Vote])],
   providers: [BlockchainService],
   controllers: [BlockchainController]
 })
--- src/blockchain/blockchain.service.ts
+++ src/blockchain/blockchain.service.ts
@@ -1,11 +1,13 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, Logger, OnModuleInit } from '@nestjs/common';
 import { Contract, id, WebSocketProvider } from 'ethers';
 import * as BalanceGameJson from './abi/BalanceGame.json';
 import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Game } from 'src/game/entity/game.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { User } from 'src/auth/entity/user.entity';
+import { Vote } from 'src/game/entity/vote.entity';
+import { VoteOption } from 'src/game/enum/vote-option.enum';
 
 /**
  *
@@ -16,6 +18,7 @@ import { User } from 'src/auth/entity/user.entity';
  * 4. 내 정보 반환할때 투표 및 상대방 조회기능도 만들기
  * 5. 백엔드 API 업데이트 하기 
  * 6. Redis 캐시화 하기 
+ * 7. Nest 배치로 주기적으로 조회하기
  * 
  */
 
@@ -30,6 +33,7 @@ export class BlockchainService implements OnModuleInit {
     private readonly gameRepo: Repository<Game>,
     @InjectRepository(User)
     private readonly userRepo: Repository<User>,
+    private readonly dataSource: DataSource
   ) {}
 
   private readonly logger = new Logger(BlockchainService.name);
@@ -53,28 +57,73 @@ export class BlockchainService implements OnModuleInit {
   }
 
   listenToEvents() {
-    this.contract.on('NewGame', async (gameId, questionA, questionB, deadline, creator) => {
+    this.contract.on('NewGame', async (gameId: number, questionA: string, questionB: string, deadline: number, creator: string) => {
       const deadlineToDate = new Date(Number(deadline) * 1000);
-      const user = await this.userRepo.findOne({
-        where: { address: creator },
-        select: { id: true }
-      });
 
-      if (!user) {
-        this.logger.warn("GameSaveError: unknown Address " + creator);
-      }
-      else {
-        const game = this.gameRepo.create({
-          id: gameId,
-          optoinA: questionA,
-          optionB: questionB,
-          deadline: deadlineToDate,
-          createdBy: user.id
+      const queryRunner = this.dataSource.createQueryRunner();
+      await queryRunner.connect();
+      await queryRunner.startTransaction();
+
+      try {
+        const user = await this.userRepo.findOne({
+          where: { address: creator },
+          select: { id: true }
         });
 
-        this.gameRepo.save(game);
-        this.logger.log("GameSaveSuccess: GameId " + gameId);
+        if (!user) {
+          this.logger.warn("GameSaveError: unknown Address " + creator);
+        }
+        else {
+          const game = this.gameRepo.create({
+            id: gameId,
+            optoinA: questionA,
+            optionB: questionB,
+            deadline: deadlineToDate,
+            createdBy: user.id
+          });
+
+          this.gameRepo.save(game);
+          this.logger.log("GameSaveSuccess: GameId " + gameId);
+        }
+
+        await queryRunner.commitTransaction();
+      } catch(err) {
+        await queryRunner.rollbackTransaction();
+        console.error(err);
+        throw new InternalServerErrorException("게임정보 저장중 오류가 발생했습니다.");
       }
     });
+
+    this.contract.on('NewVote', async (gameId: number, address: string, voteOpttion: number) => {
+      const queryRunner = this.dataSource.createQueryRunner();
+      await queryRunner.connect();
+      await queryRunner.startTransaction();
+
+      try {
+        const user = await queryRunner.manager.findOne(User, { 
+          where: { address: address }
+        });
+
+        if (!user) {
+          this.logger.warn("VoteSaveError: unknown Address " + address);
+        }
+        else {
+          const vote = queryRunner.manager.create(Vote, {
+            gameId: gameId,
+            userId: user.id,
+            option: voteOpttion == 0 ? VoteOption.A : VoteOption.B
+          });
+
+          const voteResult = await queryRunner.manager.save(vote);
+          this.logger.log("VoteSaveSuccess: VoteId " + voteResult.id);
+        }
+
+        await queryRunner.commitTransaction();
+      } catch(err) {
+        await queryRunner.rollbackTransaction();
+        console.error(err);
+        throw new InternalServerErrorException("투표 정보 저장중 오류가 발생했습니다.");
+      }
+    })
   }
 }
