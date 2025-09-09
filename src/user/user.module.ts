19d6f4f391f0b20e7c7d7f7351acce027ae61246
kangbaekho <kbh081004@gmail.com>
2025-09-09 10:10:24 +0900

    feat: 상금 수령내역 조회 기능 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -7,9 +7,10 @@ import { User } from 'src/auth/entity/user.entity';
 import { ProfileImage } from './entity/profile-image.entity';
 import { R2Service } from 'src/infrastructure/r2.service';
 import { HttpModule } from '@nestjs/axios';
+import { GameWinner } from 'src/game/entity/game-winner.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Vote, User, ProfileImage]), HttpModule],
+  imports: [TypeOrmModule.forFeature([Vote, User, ProfileImage, GameWinner]), HttpModule],
   controllers: [UserController],
   providers: [UserService, R2Service]
 })
