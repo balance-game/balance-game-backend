c8c04414478c8b0a13378e8738ca3d0c1d191158
kangbaekho <kbh081004@gmail.com>
2025-09-25 09:37:36 +0900

    fix: 쿠키 sameSite 속성 추가

--- src/auth/interceptor/jwt.interceptor.ts
+++ src/auth/interceptor/jwt.interceptor.ts
@@ -12,6 +12,7 @@ export class jwtInterceptor implements NestInterceptor {
                 res.cookie('refreshTokenId', jwt.refreshTokenId, {
                     httpOnly: true,
                     secure: true,
+                    sameSite: 'none',
                     maxAge: 1000 * 60 * 60 * 24 * 30 // 30일
                 });
 
